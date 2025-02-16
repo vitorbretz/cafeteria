9080bbb155ab7ea17b62527556dbf476c74d75f9
vitorbretz <vitorbretz28@gmail.com>
2025-02-16 14:46:20 -0300

    parte de feedback terminado
    junto com slide js

---
+++ src/scripts/script.js
@@ -0,0 +1,29 @@
+let btnRight = document.querySelector('.btn-right')
+let btnLeft = document.querySelector('.btn-left')
+let opnioes  = document.querySelectorAll('.opnion')
+let indice = 0;
+
+function mostrarOpiniao(){
+    opnioes.forEach((opnioes) => opnioes.style.display = 'none');
+    
+    opnioes[indice].style.display = 'block';
+}
+
+btnRight.addEventListener('click',(e)=>{
+    if(indice === opnioes.length - 1){
+        indice = 0;
+    }else{
+        indice++;
+    }
+    mostrarOpiniao();
+})
+btnLeft.addEventListener('click',(e)=>{
+    if(indice === 0){
+        indice = opnioes.length - 1;
+    } else if(indice === 0){
+        indice --;
+    }
+    mostrarOpiniao();
+})
+
+mostrarOpiniao();
